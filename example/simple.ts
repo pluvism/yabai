e993ce50120e5374286733c3c11c6bc5def267f9
Pluvi <mayberull@gmail.com>
2025-10-01 04:56:17 +0000

    example: callback on connected

--- example/simple.ts
+++ example/simple.ts
@@ -2,5 +2,5 @@ import { Yabai } from '../src/index.js'
 
 new Yabai()
     .cmd('ping', 'Pong!')
-    .connect()
+    .connect(() => console.log('Bot Connected!'))
     .catch((e) => console.log('Simple bot error:', e))
